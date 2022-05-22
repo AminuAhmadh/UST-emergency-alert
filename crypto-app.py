042da3cefbd0e5e2b972cea59db5b82eb4bb3198
Aminu Ahmadh <ameenuhamad@gmail.com>
2022-05-22 05:46:37 +0100

    scrape instead of using API

--- crypto-app.py
+++ crypto-app.py
@@ -6,11 +6,11 @@ import time
 import os
 from dotenv import load_dotenv
 import requests
-from requests.sessions import Session
-import json
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
+# get creds and env variables
 KEY = os.getenv("KEY")
 SECRET = os.getenv("SECRET")
 MY_TOKEN = os.getenv("MY_TOKEN")
@@ -23,30 +23,24 @@ sms = vonage.Sms(client)
 
 current_time = datetime.now()
 
+# func to send telegram message
 def send_telegram(message):
     requests.post(
         'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=markdown'.format(MY_TOKEN, 786259592, message))
 
-
-
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': API_KEY,
-}
-
-session = Session()
-session.headers.update(headers)
-
-url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=ust&aux=cmc_rank'
+# url to scrape price of UST
+url = 'https://www.coinmarketcap.com/currencies/terrausd'
 
 while True:
     print('Getting Price...')
-    data = json.loads(session.get(url).text)
-    price = data['data']['UST'][1]['quote']['USD']['price']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    price_value = soup.find('div', {'class': 'priceValue'})
+    price = float(price_value.text.strip('$'))
     print ('Price Obtained.')
 
     if price > 0.90:
-        send_telegram(f'Terra USD (UST) Price now at {price}. SELL IMMEDIATELY !!!')
+        send_telegram(f'Terra USD (UST) Price now at ${price}. SELL IMMEDIATELY !!!')
         responseData = sms.send_message({
         "from": "EMERGENCY ALERT",
         "to": SMS_TO,
@@ -59,9 +53,9 @@ while True:
         print('EMERGENCY sell alert sent')
 
     else:
-        send_telegram(f'Terra USD (UST) price now at {price}. Price below sell target. HODL!')
+        send_telegram(f'Terra USD (UST) price now at ${price}. Price below sell target. HODL!')
         print('EMERGENCY HODL sent.')
     
     print('Done for the Hour', str(current_time.hour))
-
+    # wait 1 HOUR
     time.sleep(5200)
